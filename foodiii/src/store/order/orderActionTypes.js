336b16728e025f9f55e42f31a76ef645977c878e
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2024-03-13 15:18:08 +0530

    changing this repo

--- foodiii/src/store/order/orderActionTypes.js
+++
@@ -1,8 +0,0 @@
-const orderActionTypes={
-    SET_ORDER_STATUS:'order/setOrderStatus',
-    SET_ALL_ORDERS:'order/setAllOrders',
-    SET_RESTURENT_ORDERS:'order/setResturentOrders',
-
-}
-
-export default orderActionTypes;
--- foodiii/src/store/reducer.js
+++
@@ -1,9 +0,0 @@
-import orderReducer from "./order/orderReducer";
-import {combineReducers} from 'redux'
-
-
-const rootReducer=combineReducers({
-    order:orderReducer
-});
-
-export default rootReducer;
