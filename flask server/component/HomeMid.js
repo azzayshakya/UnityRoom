f6a72853b4dcf42ef4ca3ac72a308e4de6a1bd1b
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2024-04-08 23:14:56 +0530

    adding the pages to the component

---
+++ flask server/component/HomeMid.js
@@ -0,0 +1,32 @@
+import React from "react";
+import '../Css/HomeMid.css'
+import { Link } from "react-router-dom";
+const HomeMid=()=>{
+    
+    return(<>
+   <div className="HomeMid">
+
+    <div className="about">
+        <h1>Bringing People <span className="aboutTogether">Together </span> ,Anywhere.</h1>
+        <p>Experience seamless communication with ironclad security. Connect, share screens, and innovate together, <br />hassle-free.</p>
+        
+    </div>
+
+    {(localStorage.getItem("authToken")) ?
+    <div className="midHomeButton">
+
+    <Link to={"/CreateMeet"}><button>Make-Room</button></Link>       
+    <Link  to={"/JoinMeet"}><button className="midHomeButton2nd">Join-Room</button></Link>
+    </div>
+    :
+    <div  className="LoginRequestOnHomeMid">
+        <p>*Please  <span>Login</span> OR create a <span>new Account .</span> </p>
+    </div>
+
+    }
+    
+
+   </div>
+    </>)
+}
+export default HomeMid;
